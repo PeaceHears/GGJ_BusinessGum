9380b7dfafddedaf47bb6c502e96ed6550cae40b
Furkan Baris Duyar <furkanbarisduyar@outlook.com>
2025-01-26 01:23:09 +0000

    Add rooftop and business guy walk

--- GGJ_BusinessGum/Assets/Scripts/BusinessGuyMovement.cs
+++ GGJ_BusinessGum/Assets/Scripts/BusinessGuyMovement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class BusinessGuyMovement : MonoBehaviour
 {
     public GameObject character;
+    public GameObject roofTop;
+    public Animator animator;
+
 
     public int horizontalSpeed = 10;
     public int verticalSpeed = 2;
 
     private Rigidbody2D rigidBody;
     private Vector2 inputMovement;
+    private bool isReachedToRoofTop = false;
 
     void Start()
     {
@@ -19,16 +23,51 @@ public class BusinessGuyMovement : MonoBehaviour
 
     void Update()
     {
-        inputMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 1);
+        if (!isReachedToRoofTop)
+        {
+            inputMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 1);
+        }
     }
 
     private void FixedUpdate()
     {
         inputMovement.x *= horizontalSpeed;
-        inputMovement.y *= verticalSpeed;
+
+        if (isReachedToRoofTop)
+        {
+            inputMovement.y = 0;
+        }
+        else
+        {
+            inputMovement.y *= verticalSpeed;
+        }
 
         Vector2 delta = inputMovement * Time.fixedDeltaTime;
         Vector2 newPosition = rigidBody.position + delta;
         rigidBody.MovePosition(newPosition);
+    }
+
+    private void Walk()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + 4.0f);
+        animator.SetTrigger("isWalking");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "RoofTop")
+        {
+            isReachedToRoofTop = true;
+            Walk();
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        else if (collision.gameObject.tag == "Bird")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        else if (collision.gameObject.tag == "PaperPlane")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
     }
 }
